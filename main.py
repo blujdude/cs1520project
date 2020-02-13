1d92be2769fddf670d38f8d2aaa35029443db707
BryceRyan <bryceryan1999@gmail.com>
2020-02-13 15:58:56 -0500

    Got main.py working

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-from flask import Flask, redirect
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 
 @app.route('/')
 @app.route('/home.html')
 def root():
-    return Flask.render_template("/HTML/home.html", pagetitle = "Home")
+    return flask.render_template("home.html", pagetitle = "Home")
 
 @app.route('/login.html')
 def login_page():
-    return Flask.render_template("/HTML/login.html", pagetitle = "Login")
+    return flask.render_template("login.html", pagetitle = "Login")
 
 @app.route('/build.html')
 def build_page():
-    return Flask.render_template("/HTML/build.html", pagetitle = "Build")
+    return flask.render_template("build.html", pagetitle = "Build")
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
