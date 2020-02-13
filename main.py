e77de2bfff468c4ad9b0438d5be8e075d14d7720
BryceRyan <bryceryan1999@gmail.com>
2020-02-13 14:58:00 -0500

    Wrote a basic HTML structure in main.py

--- main.py
+++ main.py
@@ -4,9 +4,17 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home.html')
 def root():
-    return redirect("/HTML/project.html", code=302)
+    return redirect("/HTML/home.html", code=302)
 
+@app.route('/login.html')
+def login_page():
+    return redirect('/HTML/login.html', code=302)
+
+@app.route('/build.html')
+def build_page():
+    return redirect('/HTML/build.html', code=302)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
