7620c6e2ca0c017652da1436ae26639be3133e84
Lillian Turner <lmt72@pitt.edu>
2020-02-22 12:40:31 -0500

    added post request struture

--- main.py
+++ main.py
@@ -16,9 +16,26 @@ def login_page():
 def signup_page():
     return flask.render_template("signup.html", pagetitle = "Sign Up")
 
+@app.route('/register', methods=['POST'])
+def register_user():
+    username = flask.request.form.get('username')
+    password1 = flask.request.form.get('password1')
+    email = flask.request.form.get('email')
+    #passwordhash = get_password_hash(password1)
+    #lmsdatastore.save_user(user, passwordhash)
+    #flask.session['user'] = user.username
+    return flask.redirect('/build.html')
+
 @app.route('/build.html')
 def build_page():
     return flask.render_template("build.html", pagetitle = "Build")
+
+@app.route('/savebuild', methods=['POST'])
+def save_build():
+	map = flask.request.form.get('map')
+	#get username
+	#save map and username
+	return flask.redirect('/build.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
