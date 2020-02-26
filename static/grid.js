bffb715cba835c5f773555bf9839f40784d1d3ff
BryceRyan <bryceryan1999@gmail.com>
2020-02-26 13:21:19 -0500

    Added ability to specify grid size

--- static/grid.js
+++ static/grid.js
@@ -5,12 +5,18 @@ function clickGridBlock(item){
 }
 */
 
-function setupGrid(){
-    length=document.getElementById("lengthField").value;
-    width=document.getElementById("widthField").value;
 
+//Issues - onload event is not working.  Only one parameter is being expressed for some reason.
+function buildCanvas(height, length){
 
-    target=document.getElementById("contain");
-
+    var canvas=document.getElementById("map");
     
+    // 70px blocks for now
+    canvas.width=40*length;
+    canvas.height=40*height;
+
+    var ctx=canvas.getContext("2d");
+
+    ctx.fillStyle = "#42F5C2";
+    ctx.fillRect(0, 0, canvas.width, canvas.length)
 }
